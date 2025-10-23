3e2b5c622419fc4144a8bd56e5c2a4d6b7e305a9
Ahmad Hadi Abd Halim <a.hadi.a.halim@gmail.com>
2025-10-23 17:32:23 +0800

    Create sudoku_puzzle.dart

---
+++ sudoku_master/models/sudoku_puzzle.dart
@@ -0,0 +1,11 @@
+class SudokuPuzzle {
+  final List<List<int>> initial;
+  final List<List<int>> solution;
+  final String difficulty;
+
+  SudokuPuzzle({
+    required this.initial,
+    required this.solution,
+    required this.difficulty,
+  });
+}
