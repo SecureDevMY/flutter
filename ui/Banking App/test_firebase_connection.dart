c23cc7f61933da4888fa4faba874ead18d198d77
Ahmad Hadi Abd Halim <a.hadi.a.halim@gmail.com>
2025-11-01 13:32:37 +0800

    Create test_firebase_connection.dart

---
+++ ui/Banking App/test_firebase_connection.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Setup Complete!'),
+      ),
+      body: Center(
+        child: Text('Firebase is now connected!'),
+      ),
+    );
+  }
+}
